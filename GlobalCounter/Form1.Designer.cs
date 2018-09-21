d2e6db64438b6e21710478dcbcacf5eae623925e
Altrentorae <killer39800@gmail.com>
2018-09-21 07:59:45 +0100

    Counter
    Just a simple counter, increment and decrement with + and - button.
    Reset with X.
    You can also increment it with Numpad0, the program does NOT need focus
    for incrementing the count

---
+++ GlobalCounter/Form1.Designer.cs
@@ -0,0 +1,106 @@
+﻿namespace GlobalCounter {
+    partial class Form1 {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent() {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.countLabel = new System.Windows.Forms.Label();
+            this.butIncr = new System.Windows.Forms.Button();
+            this.butDecr = new System.Windows.Forms.Button();
+            this.butReset = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // countLabel
+            // 
+            this.countLabel.AutoSize = true;
+            this.countLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 72F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.countLabel.Location = new System.Drawing.Point(12, 36);
+            this.countLabel.Name = "countLabel";
+            this.countLabel.Size = new System.Drawing.Size(109, 108);
+            this.countLabel.TabIndex = 0;
+            this.countLabel.Text = "{}";
+            this.countLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // butIncr
+            // 
+            this.butIncr.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.butIncr.Location = new System.Drawing.Point(221, 21);
+            this.butIncr.Name = "butIncr";
+            this.butIncr.Size = new System.Drawing.Size(47, 37);
+            this.butIncr.TabIndex = 2;
+            this.butIncr.Text = "+";
+            this.butIncr.UseVisualStyleBackColor = true;
+            this.butIncr.Click += new System.EventHandler(this.butIncr_Click);
+            // 
+            // butDecr
+            // 
+            this.butDecr.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.butDecr.Location = new System.Drawing.Point(221, 64);
+            this.butDecr.Name = "butDecr";
+            this.butDecr.Size = new System.Drawing.Size(47, 37);
+            this.butDecr.TabIndex = 3;
+            this.butDecr.Text = "-";
+            this.butDecr.UseVisualStyleBackColor = true;
+            this.butDecr.Click += new System.EventHandler(this.butDecr_Click);
+            // 
+            // butReset
+            // 
+            this.butReset.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.butReset.Location = new System.Drawing.Point(221, 107);
+            this.butReset.Name = "butReset";
+            this.butReset.Size = new System.Drawing.Size(47, 37);
+            this.butReset.TabIndex = 4;
+            this.butReset.Text = "X";
+            this.butReset.UseVisualStyleBackColor = true;
+            this.butReset.Click += new System.EventHandler(this.butReset_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(280, 164);
+            this.Controls.Add(this.butReset);
+            this.Controls.Add(this.butDecr);
+            this.Controls.Add(this.butIncr);
+            this.Controls.Add(this.countLabel);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
+            this.Text = "Global Counter";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label countLabel;
+        private System.Windows.Forms.Button butIncr;
+        private System.Windows.Forms.Button butDecr;
+        private System.Windows.Forms.Button butReset;
+    }
+}
+
---
+++ GlobalCounter/Form1.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using System.Runtime.InteropServices;
+
+namespace GlobalCounter {
+    public partial class Form1 : Form {
+
+        public Form1() {
+            InitializeComponent();
+            main();
+            ghk = new KeyHandler(Keys.NumPad0, this);
+            ghk.Register();
+        }
+        int count;
+
+        private KeyHandler ghk;
+
+        public void main() {
+            string fileCount = System.IO.File.ReadAllText(@"count.txt");
+            Int32.TryParse(fileCount, out count);
+            countLabel.Text = count.ToString();
+        }
+
+        private void butIncr_Click(object sender, EventArgs e) {
+            count++;
+            countLabel.Text = count.ToString();
+            saveNum();
+        }
+
+        private void butDecr_Click(object sender, EventArgs e) {
+            count--;
+            countLabel.Text = count.ToString();
+            saveNum();
+        }
+        private void saveNum() {
+            System.IO.File.WriteAllText(@"count.txt", count.ToString());
+        }
+
+        private void HandleHotkey() {
+            count++;
+            countLabel.Text = count.ToString();
+            saveNum();
+        }
+
+        protected override void WndProc(ref Message m) {
+            if (m.Msg == Constants.WM_HOTKEY_MSG_ID)
+                HandleHotkey();
+            base.WndProc(ref m);
+        }
+
+        private void butReset_Click(object sender, EventArgs e) {
+            DialogResult confirm = MessageBox.Show("This will reset the counter, Are you sure?", "Reset counter?", MessageBoxButtons.YesNo);
+            if(confirm == DialogResult.Yes) { count = 0; countLabel.Text = count.ToString(); saveNum(); }
+        }
+    }
+
+    public static class Constants {
+        //windows message id for hotkey
+        public const int WM_HOTKEY_MSG_ID = 0x0312;
+    }
+
+    public class KeyHandler {
+        [DllImport("user32.dll")]
+        private static extern bool RegisterHotKey(IntPtr hWnd, int id, int fsModifiers, int vk);
+
+        [DllImport("user32.dll")]
+        private static extern bool UnregisterHotKey(IntPtr hWnd, int id);
+
+        private int key;
+        private IntPtr hWnd;
+        private int id;
+
+        public KeyHandler(Keys key, Form form) {
+            this.key = (int)key;
+            this.hWnd = form.Handle;
+            id = this.GetHashCode();
+        }
+
+        public override int GetHashCode() {
+            return key ^ hWnd.ToInt32();
+        }
+
+        public bool Register() {
+            return RegisterHotKey(hWnd, id, 0, key);
+        }
+
+        public bool Unregiser() {
+            return UnregisterHotKey(hWnd, id);
+        }
+    }
+}
